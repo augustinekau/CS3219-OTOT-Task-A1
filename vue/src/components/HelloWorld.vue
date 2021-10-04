8878b14a70e3ec2122c6b05b60d33ea25d7b23e9
augustinekau <augustinekau@gmail.com>
2021-10-04 21:24:49 +0800

    First push.

---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>Docker NGINX Reverse proxy works!!</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
